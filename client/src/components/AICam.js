981d8d11eaf39e69dfe6aae4eb24280553ef6f0a
ConceptCodes <conceptcodes@gmail.com>
2019-11-03 11:32:27 -0500

    hopefully the api works

--- client/src/components/AICam.js
+++ client/src/components/AICam.js
@@ -3,49 +3,41 @@ import '../styles/AICam.css'
 import ImagePreview from './ImagePreview'
 import * as tf from '@tensorflow/tfjs'
 import Camera, { FACING_MODES } from 'react-html5-camera-photo';
+import jpeg from 'jpeg-js'
 import 'react-html5-camera-photo/build/css/index.css';
 
 
 class AICam extends React.Component {
-  constructor (props, context) {
-    super(props, context);
+  constructor (props) {
+    super(props);
     this.state = { dataUri: null, image: null, mode: FACING_MODES.ENVIRONMENT };
     this.onTakePhotoAnimationDone = this.onTakePhotoAnimationDone.bind(this);
-    this.makeImage = this.makeImage.bind(this);
-    console.log(this.props.model)
+    this.makeImage = this.makeImage.bind(this)
   }
  
   onTakePhotoAnimationDone (dataUri) {
     this.setState({ dataUri })
+    this.makeImage()
   }
 
   makeImage() {
-    let outputSize = 500;
-    let image = new Image();
-    image.onload = () => {
-      const canvas = document.createElement('canvas');
-      canvas.width = outputSize;
-      canvas.height = outputSize;
-      const context = canvas.getContext('2d');
-      context.drawImage(image, 0, 0, outputSize, outputSize);
-
-      let img =  context.getImageData(0, 0, outputSize, outputSize);
-    };
-    
-    image.src = this.state.dataUri;
-
-    //this.predict(image);
+    let img = jpeg.encode(new Blob([this.state.dataUri],{type: 'image/jpeg'}))
+    img.width = 500;
+    img.height = 500;
+    this.setState({image: img})
   }
   
 
-  predict = (img)=> tf.tidy(() => {
+  predict = ()=> tf.tidy(() => {
     const raw = tf.browser.fromPixels(this.state.image,1)
       .reshape([1, 28, 28, 1])
       .cast('float32')
       .div(255);
     
     let predictionResult =  this.props.model.predict(raw).dataSync();
+    console.log(predictionResult);
     let recognizedClothing = predictionResult.indexOf(Math.max(...predictionResult));
+    console.log(recognizedClothing);
     let labelNames = ["top", "trouser", "pullover", "dress", "coat", "sandal", "shirt", "sneaker", "bag", "ankle boot"];
     return labelNames[Math.floor(recognizedClothing)];
   })
@@ -70,7 +62,7 @@ class AICam extends React.Component {
         <br></br>
        {
           (this.state.dataUri)
-            ? <ImagePreview city={this.props.city} dataUri={this.state.dataUri} />
+            ? <ImagePreview city={this.props.city} pred={'hello'} dataUri={this.state.dataUri} />
             : <Camera onTakePhotoAnimationDone={this.onTakePhotoAnimationDone} />
         }
       </div>
